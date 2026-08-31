aaef104845b1c21a45322ca3b1f28b94e5101544
Deeds87 <118107301+Deeds87@users.noreply.github.com>
2026-08-31 13:31:12 +0200

    add: dms-greeter aggregator

---
+++ environments/display-manager/dms-greeter/default.nix
@@ -0,0 +1,22 @@
+# file: environments/display-manager/dms-greeter/default.nix
+
+# #############################################################################
+#
+# Description:
+# Aggregates DMS-greeter.
+#
+# #############################################################################
+
+{ ... }:
+
+{
+
+  nixosModules = [
+
+    ./nixos/dms-greeter.nix
+
+  ];
+
+  hmModules = [ ];
+
+}
