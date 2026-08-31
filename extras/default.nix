6ce91f6332d8e3df797e9b35deebe000961a7de5
Deeds87 <118107301+Deeds87@users.noreply.github.com>
2026-08-31 14:51:09 +0200

    chore: extras bootstrap (#11)
    * add: extras aggregator
    
    * add: .gitkeep

---
+++ extras/default.nix
@@ -0,0 +1,21 @@
+# file: extras/default.nix
+
+# #############################################################################
+#
+# Description:
+# Aggreates all personal additions and customizations to the existing system,
+# such as custom XKB layouts, icon sets, themes,
+# or other small configuration extensions that are specific to your setup.
+#
+# extras/ is not intended for additional applications, system services
+# or system features. Those belong in their respective directories.
+#
+# #############################################################################
+
+{
+
+  nixosModules = [ ];
+
+  hmModules = [ ];
+
+}
