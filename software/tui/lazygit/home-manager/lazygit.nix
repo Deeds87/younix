a72236be48dcfb2d9bf039b57e9335032157c314
Deeds87 <118107301+Deeds87@users.noreply.github.com>
2026-09-01 07:41:42 +0200

    add: lazygit

---
+++ software/tui/lazygit/home-manager/lazygit.nix
@@ -0,0 +1,18 @@
+# file: software/tui/lazygit/home-manager/lazygit.nix
+
+# #############################################################################
+#
+# Description:
+# Lazygit - A git TUI frontend
+#
+# #############################################################################
+
+{ ... }:
+
+{
+
+  programs.lazygit = {
+    enable = true;
+  };
+
+}
