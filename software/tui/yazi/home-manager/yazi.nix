6d483c13eda2b7049e3a94df773d11223a7f3bcc
Deeds87 <118107301+Deeds87@users.noreply.github.com>
2026-09-04 08:45:49 +0200

    add: gvfs plugin

--- software/tui/yazi/home-manager/yazi.nix
+++ software/tui/yazi/home-manager/yazi.nix
@@ -7,7 +7,7 @@
 #
 # #############################################################################
 
-{ ... }:
+{ pkgs, ... }:
 
 {
 
@@ -16,6 +16,66 @@
     enable = true;
     enableZshIntegration = true;
     shellWrapperName = "y";
+
+    plugins = with pkgs; {
+      gvfs = {
+        package = yaziPlugins.gvfs;
+        setup = true;
+      };
+    };
+
+    keymap = {
+      mgr.prepend_keymap = [
+        {
+          on = [
+            "M"
+            "m"
+          ];
+          run = "plugin gvfs -- select-then-mount --jump";
+          desc = "Mount GVFS device/share";
+        }
+        {
+          on = [
+            "M"
+            "u"
+          ];
+          run = "plugin gvfs -- select-then-unmount";
+          desc = "Unmount GVFS device/share";
+        }
+        {
+          on = [
+            "M"
+            "a"
+          ];
+          run = "plugin gvfs -- add-mount";
+          desc = "Add GVFS mount";
+        }
+        {
+          on = [
+            "M"
+            "e"
+          ];
+          run = "plugin gvfs -- edit-mount";
+          desc = "Edit GVFS mount";
+        }
+        {
+          on = [
+            "M"
+            "r"
+          ];
+          run = "plugin gvfs -- remove-mount";
+          desc = "Remove GVFS mount";
+        }
+        {
+          on = [
+            "g"
+            "m"
+          ];
+          run = "plugin gvfs -- jump-to-device";
+          desc = "Jump to GVFS device";
+        }
+      ];
+    };
 
   };
 
