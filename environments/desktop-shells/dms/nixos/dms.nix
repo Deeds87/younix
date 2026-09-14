991c8ecdad465beeb4f294db4a106eb30104a29e
Deeds87 <118107301+Deeds87@users.noreply.github.com>
2026-09-14 06:52:32 +0200

    remove: deprecated feature settings

--- environments/desktop-shells/dms/nixos/dms.nix
+++ environments/desktop-shells/dms/nixos/dms.nix
@@ -39,12 +39,10 @@
     };
 
     # ------------------------------------ Feature settings
-    enableSystemMonitoring = true; # System monitoring widgets (dgop)
     enableVPN = true; # VPN management widget
     enableDynamicTheming = true; # Wallpaper-based theming (matugen)
     enableAudioWavelength = true; # Audio visualizer (cava)
     enableCalendarEvents = false; # Calendar integration (khal)
-    enableClipboardPaste = true; # Pasting from the clipboard history (wtype)
 
     # --------------------------------------------- Plugins
     plugins = {
