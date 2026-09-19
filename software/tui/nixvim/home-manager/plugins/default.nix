ba0c38e5fb0cf0e7f82b1839f8d56afd37004690
Deeds87 <118107301+Deeds87@users.noreply.github.com>
2026-09-19 10:43:50 +0200

    update: plugin aggregator

--- software/tui/nixvim/home-manager/plugins/default.nix
+++ software/tui/nixvim/home-manager/plugins/default.nix
@@ -13,8 +13,14 @@
 
   imports = [
 
+    ./treesitter.nix # Manage tree-sitter-parsers
     ./which-key.nix # Popup for keymaps
+    ./mini-icons.nix # Get icons
+    ./bufferline.nix # Show buffers as tabs
+    ./lualine.nix # Show a nicer statusline
     ./yazi.nix # Filemanager
+    ./blink-pairs.nix # Autocomplete delimiter pairs
+    ./todo-comments.nix # Make todo comments available
 
   ];
 
