6b4fdc45ba9265c778217616d9f2d10d981c51b1
Deeds87 <118107301+Deeds87@users.noreply.github.com>
2026-09-20 09:53:21 +0200

    add: plugins

--- software/tui/nixvim/home-manager/plugins/default.nix
+++ software/tui/nixvim/home-manager/plugins/default.nix
@@ -14,11 +14,13 @@
   imports = [
 
     ./treesitter.nix # Manage tree-sitter-parsers
+    ./snacks.nix # Different QoL plugins
     ./which-key.nix # Popup for keymaps
     ./mini-icons.nix # Get icons
+    ./noice.nix # Modern UI
     ./bufferline.nix # Show buffers as tabs
     ./lualine.nix # Show a nicer statusline
-    ./yazi.nix # Filemanager
+    # ./yazi.nix # Filemanager
     ./blink-pairs.nix # Autocomplete delimiter pairs
     ./todo-comments.nix # Make todo comments available
 
