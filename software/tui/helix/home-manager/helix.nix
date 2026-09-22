dc1fe121e388eac05ceb296bc5605162f9fcb7b8
Deeds87 <118107301+Deeds87@users.noreply.github.com>
2026-09-22 09:55:20 +0200

    refactor: move sections to dedicated files

--- software/tui/helix/home-manager/helix.nix
+++ software/tui/helix/home-manager/helix.nix
@@ -5,68 +5,53 @@
 # Description:
 # Helix - Vim like modal editor.
 #
-# Dependencies:
-# Yazi    - Filemanager
-# Lazygit - Git frontend
-# Kitty   - Terminal emulator
-#
-# These programs has to be installed when using the given keymaps.
-#
-# TODO: Datei aufteilen (keymaps, options, lsp, usw.)
-#
 # #############################################################################
 
-{
-  pkgs,
-  lib,
-  osConfig,
-  inputs,
-  ...
-}:
-
-let
-
-  hostname = osConfig.networking.hostName;
-
-in
+{ pkgs, inputs, ... }:
 
 {
   programs.helix = {
 
+    # GENERAL SETUP ===========================================================
     enable = true;
 
     # Use package from master branch
     package = inputs.helix.packages."${pkgs.stdenv.hostPlatform.system}".helix;
 
+    # -------------------------------------- Extra packages
     extraPackages = with pkgs; [
       nixd # LSP for Nix
       nixfmt # Formatter for Nix
       markdown-oxide # LSP for Markdown
-      simple-completion-language-server
+      simple-completion-language-server # LSP for completions
     ];
+
+    # EDITOR CONFIGURATION ====================================================
 
     settings = {
 
-      # THEME =================================================================
-
+      # --------------------------------------------- Theme
       theme = {
         light = "ayu_light";
         dark = "ayu_dark";
       };
 
-      # GENERRAL SETTINGS =====================================================
-
       editor = {
+
+        # ----------------------------------------- General
         scrolloff = 10;
         line-number = "relative";
         color-modes = true;
-        undercurl = true;
+
+        # ------------------------------------------ Cursor
         cursorline = true;
         cursor-shape = {
           normal = "block";
           insert = "bar";
           select = "underline";
         };
+
+        # ----------------------------------------- Gutters
         gutters = [
           "diagnostics"
           "spacer"
@@ -77,10 +62,12 @@ in
           "diff"
         ];
 
+        # ------------------------------------- File-Picker
         file-picker = {
           git-ignore = false;
         };
 
+        # -------------------------------------- Whitespace
         whitespace = {
           render = {
             space = "all";
@@ -91,12 +78,15 @@ in
           };
         };
 
+        # ------------------------------------- Indentation
         indent-guides = {
           render = true;
         };
 
+        # -------------------------------------- Bufferline
         bufferline = "always";
 
+        # -------------------------------------- Statusline
         statusline = {
           left = [
             "mode"
@@ -106,6 +96,9 @@ in
             "spinner"
           ];
           center = [
+            "current-working-directory"
+            "spacer"
+            "separator"
             "file-name"
             "read-only-indicator"
             "file-modification-indicator"
@@ -119,6 +112,7 @@ in
           ];
         };
 
+        # ------------------------ Diagnostics and messages
         end-of-line-diagnostics = "hint";
 
         inline-diagnostics = {
@@ -132,82 +126,7 @@ in
         };
       };
 
-      # LANGUAGE SERVERS ========================================================
-
-      languages = {
-
-        # Languages -----------------------------------------
-        language = [
-          {
-            name = "nix";
-            auto-format = true;
-            language-servers = [ "nixd" ];
-            formatter = {
-              command = "${lib.getExe pkgs.nixfmt}";
-              args = [ "-" ];
-            };
-          }
-          {
-            name = "markdown";
-            language-servers = [
-              "markdown-oxide"
-              "simple-completion-language-server"
-            ];
-          }
-        ];
-
-        # Language servers ----------------------------------
-        language-server = {
-          markdown-oxide = {
-            command = "markdown-oxide";
-          };
-
-          simple-completion-language-server = {
-            command = "simple-completion-language-server";
-            config = {
-              feature_words = false;
-              feature_snippets = true;
-              snippets_first = true;
-            };
-          };
-
-          nixd = {
-            command = "nixd";
-            args = [ "--semantic-tokens=true" ];
-
-            config = {
-              nixpkgs.expr = "import (builtins.getFlake (builtins.toString ./.)).inputs.nixpkgs { }";
-              formatting.command = [ "${lib.getExe pkgs.nixfmt}" ];
-
-              options = {
-                nixos.expr = "(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.${hostname}.options";
-
-                home-manager.expr = "(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.${hostname}.options.home-manager.users.type.getSubOptions []";
-              };
-            };
-          };
-        };
-      };
     };
 
-    home.file.".config/helix/external-snippets.toml".text = ''
-      [[sources]]
-      name = "friendly-snippets"
-      git = "https://github.com/rafamadriz/friendly-snippets.git"
-
-      [[sources.paths]]
-      scope = [ "markdown" ]
-      path = "snippets/markdown.json"
-    '';
-
-    home.activation.fetchSnippets = lib.hm.dag.entryAfter [ "installPackages" ] ''
-      export PATH="${
-        lib.makeBinPath [
-          pkgs.git
-          pkgs.simple-completion-language-server
-        ]
-      }:$PATH"
-      ${pkgs.simple-completion-language-server}/bin/simple-completion-language-server fetch-external-snippets
-    '';
   };
 }
