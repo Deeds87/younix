86798780e7da6f2b57103ac66aa11146125957f2
Deeds87 <118107301+Deeds87@users.noreply.github.com>
2026-09-22 06:35:06 +0200

    fix: wrap configuration into settings attribute set

--- software/tui/helix/home-manager/keymaps.nix
+++ software/tui/helix/home-manager/keymaps.nix
@@ -13,60 +13,64 @@
 
   programs.helix = {
 
-    keys = {
+    settings = {
 
-      # NORMAL MODE ===========================================================
+      keys = {
 
-      normal = {
+        # NORMAL MODE =========================================================
 
-        # ----------------------------------------- General
-        esc = [
-          "collapse_selection"
-          "keep_primary_selection"
-        ];
+        normal = {
 
-        # ----------------------------------------- Buffers
-
-        "S-tab" = ":bp"; # Previous buffer
-        "tab" = ":bn"; # Next buffer
-
-        # SPACE MODE ==========================================================
-
-        space = {
-
-          space = "file_picker";
-
-          # -------------------------------- External tools
-          # Open Yazi filemanager
-          e = [
-            ":sh rm -f /tmp/unique-ca1ea106"
-            ":insert-output yazi '%{buffer_name}' --chooser-file=/tmp/unique-ca1ea106"
-            ":sh printf '\\x1b[?1049h\\x1b[?2004h' > /dev/tty"
-            ":open %sh{cat /tmp/unique-ca1ea106}"
-            ":redraw"
-            ":set mouse false"
-            ":set mouse true"
+          # --------------------------------------- General
+          esc = [
+            "collapse_selection"
+            "keep_primary_selection"
           ];
 
-          # Open Lazygit
-          l = [
-            ":write-all"
-            ":noop %sh{kitty @ launch --type=overlay --wait-for-child-to-exit --cwd=current lazygit}"
-            ":redraw"
-            ":reload-all"
-          ];
+          # --------------------------------------- Buffers
+
+          "S-tab" = ":bp"; # Previous buffer
+          "tab" = ":bn"; # Next buffer
+
+          # SPACE MODE ========================================================
+
+          space = {
+
+            space = "file_picker";
+
+            # ------------------------------ External tools
+            # Open Yazi filemanager
+            e = [
+              ":sh rm -f /tmp/unique-ca1ea106"
+              ":insert-output yazi '%{buffer_name}' --chooser-file=/tmp/unique-ca1ea106"
+              ":sh printf '\\x1b[?1049h\\x1b[?2004h' > /dev/tty"
+              ":open %sh{cat /tmp/unique-ca1ea106}"
+              ":redraw"
+              ":set mouse false"
+              ":set mouse true"
+            ];
+
+            # Open Lazygit
+            l = [
+              ":write-all"
+              ":noop %sh{kitty @ launch --type=overlay --wait-for-child-to-exit --cwd=current lazygit}"
+              ":redraw"
+              ":reload-all"
+            ];
+
+          };
 
         };
 
+        # INSERT MODE =========================================================
+
+        insert = { };
+
+        # SELECT MODE =========================================================
+
+        select = { };
+
       };
-
-      # INSERT MODE ===========================================================
-
-      insert = { };
-
-      # SELECT MODE ===========================================================
-
-      select = { };
 
     };
 
